1500bf99e72f9851584a0c698943a4234b1521e8
TobiA34 <36420903+TobiA34@users.noreply.github.com>
2023-10-28 12:02:03 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const saveBtn = document.getElementById("input-btn")
+
+function saveLead(){
+      console.log("Button clicked!")
+}
+
+saveBtn.addEventListener("click", saveLead)
+
